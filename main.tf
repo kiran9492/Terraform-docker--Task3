e4c1c9d90cd649e6c430cf95ebc53a2e4fdfd8ba
kiran9492 <ayanchakiran06@gmail.com>
2025-12-24 21:04:30 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull Docker Image
+resource "docker_image" "nginx_image" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Create Docker Container
+resource "docker_container" "nginx_container" {
+  name  = "terraform-nginx"
+  image = docker_image.nginx_image.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
